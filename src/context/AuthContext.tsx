30e22a5c2a0bc3c4294239b211fdcdc36cde5a50
TechmoNoway <cuty0406@outlook.com>
2024-01-17 22:39:52 +0700

    fix page

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -69,9 +69,8 @@ const AuthProvider = ({ children }: { children: React.ReactNode }) => {
 
   useEffect(() => {
     if (
-      localStorage.getItem("cookieFallback") === "[]"
-    //    ||
-    //   localStorage.getItem("cookieFallback") === null
+      localStorage.getItem("cookieFallback") === "[]" ||
+      localStorage.getItem("cookieFallback") === null
     ) {
       navigate("/sign-in");
     }
